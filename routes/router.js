2c2a9a63582681e0185d180385257b662bed002d
Siberianeagle5 <christopherdavyydoconnell.com>
2019-01-03 19:00:26 -0500

    set up the db and the server to scrape what I want

---
+++ routes/router.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio'),
+      axios = require('axios');
+
+module.exports = function(app) {
+    // the route for the homepage
+    app.get("/", (req, res) => {
+        res.render("index");
+    });
+    // the route for scraping https://physicsworld.com/
+    app.get("/scrape", (req, res) => {
+        // first we grab the body of the html with axios
+        axios.get("https://physicsworld.com/").then(response => {
+            // then we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(response.data);
+            // grab every h3 within an article tag and do the following:
+            $("article.listing-block").each((i, element) => {
+                // save an empty result object
+                var result = {};
+                // add the headline to the result object
+                result.headline = $(element).find("b").text();
+                // add the link to the original article to the result object
+                result.link = $(element).find("a").attr("href");
+                // add the summary to the result object
+                result.summary = $(element).find("p").text();
+                console.log(result);
+            });
+            res.redirect("/");
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-// require the node packages for the app
-const express = require("express"),
-      mongoose = require("mongoose"),
-      axios = require("axios"),
-      cheerio = require("cheerio"),
-      db = require(""),
-      PORT = 3000,
+const express = require('express'),
+      PORT = process.env.PORT || 3000,
+      exphbs = require('express-handlebars'),
+      mongoose = require('mongoose'),
       app = express();
 
-// configure the application middleware
+// Set up the application middleware
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-// connect to the Mongo DB
+// Set up handlebars as the viewing engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// connect to the mongo db
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Routes
+require("./routes/router.js")(app);
+
+app.listen(PORT, () => console.log("Server listening on http://localhost:" + PORT));
